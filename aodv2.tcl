3572f5bc0ede3c9a7b3f189601f3a6e977f2f07b
AJMAL SHA <102836235+ajuuka1999@users.noreply.github.com>
2023-01-08 09:56:48 +0000

    Add files via upload

---
+++ aodv2.tcl
@@ -0,0 +1,215 @@
+#===================================
+#     Simulation parameters setup
+#===================================
+set val(chan)   Channel/WirelessChannel    ;# channel type
+set val(prop)   Propagation/TwoRayGround   ;# radio-propagation model
+set val(netif)  Phy/WirelessPhy            ;# network interface type
+set val(mac)    Mac/802_11                 ;# MAC type
+set val(ifq)    Queue/DropTail/PriQueue    ;# interface queue type
+set val(ll)     LL                         ;# link layer type
+set val(ant)    Antenna/OmniAntenna        ;# antenna model
+set val(ifqlen) 10                         ;# max packet in ifq
+set val(nn)     14                         ;# number of mobilenodes
+set val(rp)     AODV                       ;# routing protocol
+set val(x)      1301                       ;# X dimension of topography
+set val(y)      601                        ;# Y dimension of topography
+set val(stop)   40.0                       ;# time of simulation end
+
+#===================================
+#        Initialization        
+#===================================
+#Create a ns simulator
+set ns [new Simulator]
+
+#Setup topography object
+set topo       [new Topography]
+$topo load_flatgrid $val(x) $val(y)
+set god [create-god $val(nn)]
+
+#Open the NS trace file
+set tracefile [open aodv2.tr w]
+$ns trace-all $tracefile
+
+
+#Open the NAM trace file
+set namfile [open aodv2.nam w]
+$ns namtrace-all $namfile
+$ns namtrace-all-wireless $namfile $val(x) $val(y)
+set chan [new $val(chan)];#Create wireless channel
+
+#===================================
+#     Mobile node parameter setup
+#===================================
+$ns node-config -adhocRouting  $val(rp) \
+                -llType        $val(ll) \
+                -macType       $val(mac) \
+                -ifqType       $val(ifq) \
+                -ifqLen        $val(ifqlen) \
+                -antType       $val(ant) \
+                -propType      $val(prop) \
+                -phyType       $val(netif) \
+                -channel       $chan \
+                -topoInstance  $topo \
+                -agentTrace    ON \
+                -routerTrace   ON \
+                -macTrace      ON \
+                -movementTrace ON
+
+#===================================
+#        Nodes Definition        
+#===================================
+#Create 14 nodes
+set n0 [$ns node]
+$n0 set X_ 101
+$n0 set Y_ 500
+$n0 set Z_ 0.0
+$ns initial_node_pos $n0 20
+set n1 [$ns node]
+$n1 set X_ 203
+$n1 set Y_ 499
+$n1 set Z_ 0.0
+$ns initial_node_pos $n1 20
+set n2 [$ns node]
+$n2 set X_ 300
+$n2 set Y_ 501
+$n2 set Z_ 0.0
+$ns initial_node_pos $n2 20
+set n3 [$ns node]
+$n3 set X_ 395
+$n3 set Y_ 500
+$n3 set Z_ 0.0
+$ns initial_node_pos $n3 20
+set n4 [$ns node]
+$n4 set X_ 1102
+$n4 set Y_ 77
+$n4 set Z_ 0.0
+$ns initial_node_pos $n4 20
+set n5 [$ns node]
+$n5 set X_ 1004
+$n5 set Y_ 78
+$n5 set Z_ 0.0
+$ns initial_node_pos $n5 20
+set n6 [$ns node]
+$n6 set X_ 951
+$n6 set Y_ 77
+$n6 set Z_ 0.0
+$ns initial_node_pos $n6 20
+set n7 [$ns node]
+$n7 set X_ 876
+$n7 set Y_ 77
+$n7 set Z_ 0.0
+$ns initial_node_pos $n7 20
+set n8 [$ns node]
+$n8 set X_ 603
+$n8 set Y_ 129
+$n8 set Z_ 0.0
+$ns initial_node_pos $n8 20
+set n9 [$ns node]
+$n9 set X_ 601
+$n9 set Y_ 466
+$n9 set Z_ 0.0
+$ns initial_node_pos $n9 20
+set n10 [$ns node]
+$n10 set X_ 696
+$n10 set Y_ 265
+$n10 set Z_ 0.0
+$ns initial_node_pos $n10 20
+set n11 [$ns node]
+$n11 set X_ 491
+$n11 set Y_ 323
+$n11 set Z_ 0.0
+$ns initial_node_pos $n11 20
+set n12 [$ns node]
+$n12 set X_ 726
+$n12 set Y_ 265
+$n12 set Z_ 0.0
+$ns initial_node_pos $n12 20
+set n13 [$ns node]
+$n13 set X_ 541
+$n13 set Y_ 293
+$n13 set Z_ 0.0
+$ns initial_node_pos $n13 20
+
+
+$god new_node $n0
+$n0 color black
+$god new_node $n1
+$n1 color black
+$god new_node $n2
+$n2 color black
+$god new_node $n3
+$n3 color black
+$god new_node $n4
+$n4 color black
+$god new_node $n5
+$n5 color black
+$god new_node $n6
+$n6 color black
+$god new_node $n7
+$n7 color black
+$god new_node $n8
+$n8 color black
+$god new_node $n9
+$n9 color black
+$god new_node $n10
+$n10 color black
+$god new_node $n11
+$n11 color black
+$god new_node $n12
+$n12 color black
+$god new_node $n13
+$n13 color black
+
+#===================================
+#        Agents Definition        
+#===================================
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n8 $tcp0
+set sink0 [new Agent/TCPSink]
+$ns attach-agent $n9 $sink0
+$ns connect $tcp0 $sink0
+$tcp0 set packetSize_ 1700
+#===================================
+#        Applications Definition        
+#===================================
+#Setup a FTP Application over TCP connection
+set ftp0 [new Application/FTP]
+$ftp0 attach-agent $tcp0
+$ns at 4.0 "$ftp0 start"
+$ns at 39.5 "$ftp0 stop"
+#===================================
+#        Node Movement    
+#===================================
+$ns at 1.0 "$n0 setdest 1200 500 15"
+$ns at 1.0 "$n1 setdest 1175 500 15"
+$ns at 1.0 "$n2 setdest 1150 500 15"
+$ns at 1.0 "$n3 setdest 1125 500 15"
+$ns at 1.0 "$n4 setdest 50 75 15"
+$ns at 1.0 "$n5 setdest 75 75 15"
+$ns at 1.0 "$n6 setdest 100 75 15"
+$ns at 1.0 "$n7 setdest 125 75 15"
+$ns at 1.0 "$n8 setdest 1200 129 15"
+$ns at 1.0 "$n9 setdest 1200 466 13"
+$ns at 1.0 "$n10 setdest 50 265 12"
+$ns at 1.0 "$n11 setdest 1200 323 13"
+$ns at 1.0 "$n13 setdest 1250 293 20"
+
+#===================================
+#        Termination        
+#===================================
+#Define a 'finish' procedure
+proc finish {} {
+    global ns tracefile namfile
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    exec nam aodv2.nam &
+    exit 0
+}
+for {set i 0} {$i < $val(nn) } { incr i } {
+    $ns at $val(stop) "\$n$i reset"
+}
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "finish"
+$ns at $val(stop) "puts \"done\" ; $ns halt"
+$ns run
---
+++ wsna21.tcl
@@ -0,0 +1,299 @@
+
+#===================================
+#     Simulation parameters setup
+#===================================
+set val(chan)   Channel/WirelessChannel    ;# channel type
+set val(prop)   Propagation/TwoRayGround   ;# radio-propagation model
+set val(netif)  Phy/WirelessPhy            ;# network interface type
+set val(mac)    Mac/802_11                 ;# MAC type
+set val(ifq)    Queue/DropTail/PriQueue    ;# interface queue type
+set val(ll)     LL                         ;# link layer type
+set val(ant)    Antenna/OmniAntenna        ;# antenna model
+set val(ifqlen) 100                        ;# max packet in ifq
+set val(nn)     13                         ;# number of mobilenodes
+set val(rp)     AODV                       ;# routing protocol
+set val(x)      1045                       ;# X dimension of topography
+set val(y)      637                        ;# Y dimension of topography
+set val(stop)   15.0                       ;# time of simulation end
+set val(energy) 100.0                      ;# node initial energy value
+set nd_()       null                       ;# Empty array to point at nodes
+
+#===================================
+#        Initialization        
+#===================================
+#Create a ns simulator
+set ns [new Simulator]
+
+#Setup topography object
+set topo       [new Topography]
+$topo load_flatgrid $val(x) $val(y)
+create-god $val(nn)
+
+#Open the NS trace file
+set tracefile [open outa.tr w]
+$ns trace-all $tracefile
+
+#Open the NAM trace file
+set namfile [open outa.nam w]
+$ns namtrace-all $namfile
+$ns namtrace-all-wireless $namfile $val(x) $val(y)
+
+
+#######################################
+#loop to initialise authentication value as zero 
+
+for {set m 0} {$m < $val(nn) } { incr m } {
+
+              set fid [open wsndata/crl/$m.txt w]
+
+              puts $fid "0"
+
+              close $fid
+            }
+
+######################################
+
+#===================================
+#     Mobile node parameter setup
+#===================================
+
+set chan [new $val(chan)];#Create wireless channel
+$ns node-config -adhocRouting  $val(rp) \
+                -llType        $val(ll) \
+                -macType       $val(mac) \
+                -ifqType       $val(ifq) \
+                -ifqLen        $val(ifqlen) \
+                -antType       $val(ant) \
+                -propType      $val(prop) \
+                -phyType       $val(netif) \
+                -channel       $chan \
+                -topoInstance  $topo \
+                -agentTrace    ON \
+                -routerTrace   ON \
+                -macTrace      ON \
+                -movementTrace ON 
+
+#===================================
+#        Nodes Definition        
+#===================================
+#Create 25 nodes
+set nd_(0) [$ns node]
+$nd_(0) set X_ 100
+$nd_(0) set Y_ 500
+$nd_(0) set Z_ 0.0
+$ns initial_node_pos $nd_(0) 40
+
+set nd_(1) [$ns node]
+$nd_(1) set X_ 100
+$nd_(1) set Y_ 350
+$nd_(1) set Z_ 0.0
+$ns initial_node_pos $nd_(1) 40
+
+set nd_(2) [$ns node]
+$nd_(2) set X_ 100
+$nd_(2) set Y_ 200
+$nd_(2) set Z_ 0.0
+$ns initial_node_pos $nd_(2) 40
+
+set nd_(3) [$ns node]
+$nd_(3) set X_ 100
+$nd_(3) set Y_ 50
+$nd_(3) set Z_ 0.0
+$ns initial_node_pos $nd_(3) 40
+
+set nd_(4) [$ns node]
+$nd_(4) set X_ 250
+$nd_(4) set Y_ 500
+$nd_(4) set Z_ 0.0
+$ns initial_node_pos $nd_(4) 40
+
+set nd_(5) [$ns node]
+$nd_(5) set X_ 250
+$nd_(5) set Y_ 350
+$nd_(5) set Z_ 0.0
+$ns initial_node_pos $nd_(5) 40
+
+set nd_(6) [$ns node]
+$nd_(6) set X_ 250
+$nd_(6) set Y_ 200
+$nd_(6) set Z_ 0.0
+$ns initial_node_pos $nd_(6) 40
+
+set nd_(7) [$ns node]
+$nd_(7) set X_ 250
+$nd_(7) set Y_ 50
+$nd_(7) set Z_ 0.0
+$ns initial_node_pos $nd_(7) 40
+
+set nd_(8) [$ns node]
+$nd_(8) set X_ 400
+$nd_(8) set Y_ 500
+$nd_(8) set Z_ 0.0
+$ns initial_node_pos $nd_(8) 40
+
+set nd_(9) [$ns node]
+$nd_(9) set X_ 400
+$nd_(9) set Y_ 350
+$nd_(9) set Z_ 0.0
+$ns initial_node_pos $nd_(9) 40
+
+set nd_(10) [$ns node]
+$nd_(10) set X_ 400
+$nd_(10) set Y_ 200
+$nd_(10) set Z_ 0.0
+$ns initial_node_pos $nd_(10) 40
+
+set nd_(11) [$ns node]
+$nd_(11) set X_ 400
+$nd_(11) set Y_ 50
+$nd_(11) set Z_ 0.0
+$ns initial_node_pos $nd_(11) 40
+
+#set nd_(12) [$ns node]
+#$nd_(12) set X_ 550
+#$nd_(12) set Y_ 500
+#$nd_(12) set Z_ 0.0
+#$ns initial_node_pos $nd_(12) 40
+
+set nd_(12) [$ns node]
+$nd_(12) set X_ 475
+$nd_(12) set Y_ 275
+$nd_(12) set Z_ 0.0
+$ns initial_node_pos $nd_(12) 40
+
+
+$ns at 0.0 "$nd_(12) label ZINK"
+$ns at 0.0 "$nd_(12) color blue"
+$nd_(12) color blue
+$ns at 0.0 "$nd_(12) add-mark m1 blue hexagon"
+
+$ns at 0.0 "$nd_(6) label DC2"
+$ns at 0.0 "$nd_(6) color green"
+$nd_(6) color green
+$ns at 0.0 "$nd_(6) add-mark m1 blue hexagon"
+
+$ns at 0.0 "$nd_(5) label Attacker"
+$ns at 0.0 "$nd_(5) color red"
+$nd_(5) color red
+$ns at 0.0 "$nd_(5) add-mark m1 red hexagon"
+
+########################################
+#       Data Generator
+########################################
+proc Randch { min max } {
+    set maxFactor [expr [expr $max + 1] - $min]
+    set value [expr int([expr rand() * 100])]
+    set value [expr [expr $value % $maxFactor] + $min]
+    #puts $value
+    
+    ######################
+    
+    set fid [open wsndata/data.txt w]
+    puts $fid $value
+    close $fid
+    
+    ######################
+
+    return $value
+    
+}
+
+#===================================
+#        Agents Definition        
+#===================================
+set udp1 [new Agent/UDP]
+set null1 [new Agent/Null]
+$ns attach-agent $nd_(12) $null1
+$ns attach-agent $nd_(6) $udp1
+$ns connect $udp1 $null1
+
+
+#===================================
+#        Applications Definition        
+#===================================
+#Setup a CBR Application over UDP connection
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 attach-agent $udp1
+$cbr0 set packetSize_ 64
+$cbr0 set rate_ 0.01Mb
+#$cbr0 set random_ null
+$ns at 1.0 "$cbr0 start"
+$ns at 3.0 "$cbr0 stop"
+
+
+proc senddat {st et dat} {
+
+  global ns nd_ 
+  
+  if { $dat <= 50} {
+    set dc 6
+    for {set i 0} {$i < 11 } { incr i } {  
+    
+    set udp [new Agent/UDP]
+    set null [new Agent/Null]
+    $ns attach-agent $nd_($i) $udp
+    $ns attach-agent $nd_($dc) $null
+    $ns connect $udp $null
+   
+
+    set cbr [new Application/Traffic/CBR]
+    $cbr attach-agent $udp
+    if {$i == 5} {
+    $cbr set packetSize_ 512
+    $cbr set rate_ 0.7Mb
+    } else {
+    $cbr set packetSize_ 64
+    $cbr set rate_ 0.01Mb
+    }
+    #$cbr0 set random_ null
+  
+    $ns at $st "$cbr start"
+    $ns at $et "$cbr stop"
+    }
+   }
+
+  return 0
+}
+
+proc readdat {} {
+  
+  set fp [open "wsndata/data.txt" r]
+  set file_data [read $fp]
+  puts $file_data
+  puts "File Data"
+  close $fp
+  return $file_data
+}
+
+
+for {set i 0} {$i < 3 } { incr i } {
+
+  set st1 [expr $i+4]
+  set et1 [expr $st1+5]
+
+  $ns at $i "set a [Randch 1 45 ]" 
+
+  $ns at $i.2 "senddat $st1 $et1 [readdat]" 
+  
+}            
+
+
+#===================================
+#        Termination        
+#===================================
+#Define a 'finish' procedure
+proc finish {} {
+    global ns tracefile namfile
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    exec nam outa.nam &
+    exit 0
+}
+for {set i 0} {$i < $val(nn) } { incr i } {
+    $ns at $val(stop) "\$nd_($i) reset"
+}
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "finish"
+$ns at $val(stop) "puts \"done\" ; $ns halt"
+$ns run
